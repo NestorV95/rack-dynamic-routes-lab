0d89763a8e2a6064778fb947a01f8445790ea41e
Nestor <nestor.venegas95@gmail.com>
2021-03-14 18:36:22 -0500

    commit tree a1669c2fa31374c27860c7b09a2015fc5b1932ab

--- app/application.rb
+++ app/application.rb
@@ -3,16 +3,18 @@ class Application
     def call(env)
         resp = Rack::Response.new
         req = Rack::Request.new(env)
-            if req.path.match(/items/)
+
+            if req.path.match(/items/) 
                 item_name = req.path.split("/items/").last
-                item = @@items.find{|i| i.name == item_name}
-                resp.write item.price
-            elsif req.path.match(/search/)
-                search_term = req.param["item"]
-                resp.write "#{search_term} is one of our items"
-            else
-                resp.write "Item not found"
-                resp.status 400
+                if item = @@items.find{|i| i.name == item_name}
+                    resp.write item.price
+                else
+                    resp.write "Item not found"
+                    resp.status = 400
+                end    
+            else 
+                resp.write "Route not found"
+                resp.status = 404
             end
        resp.finish
     end  
